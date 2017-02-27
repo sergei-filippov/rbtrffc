4905ab5633e5eac76566a9f7c01f4bf4767f811a
sergei-filippov <sergei-filippov2001@yandex.ru>
2017-02-27 07:59:08 +0300

    a little bit better code

--- distanceRead_ex1/distanceRead_ex1.ino
+++ distanceRead_ex1/distanceRead_ex1.ino
@@ -2,16 +2,16 @@ int d, rowA6; // расстояние и значения с датчика
 void setup() {
   Serial.begin(9600);
  // pinMode(A10,INPUT);
-  pinMode(A6,INPUT);
+  pinMode(A5,INPUT);
 
 }
 
 void loop() {
-  rowA6=analogRead(A6);
- Serial.print(rowA6);
-  Serial.print(" ");
- d=5222/(rowA6-13);
-  Serial.println(d);
+  rowA6=analogRead(A5);
+ Serial.println(rowA6);
+ // Serial.print(" ");
+ //d=5222/(rowA6-13);
+ // Serial.println(d);
   delay(200);
 
 }
